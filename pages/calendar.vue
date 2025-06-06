8867543e844755d062e6ee2dc5bbae468b856fd0
ebolblga <kkochanovskiy@gmail.com>
2025-06-06 23:50:46 +0300

    fix: remove extra

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -15,7 +15,6 @@ const disabledDates: DateRange[] = [
 ]
 
 const vacations: DateRange[] = [
-    { start: new Date(2025, 5, 23), end: new Date(2025, 6, 6) },
     { start: new Date(2025, 6, 28), end: new Date(2025, 7, 10) },
     { start: new Date(2025, 9, 20), end: new Date(2025, 9, 26) },
 ]
